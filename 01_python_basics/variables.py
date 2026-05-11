70a57e064a347242444b469125176372b01a4e2d
Arcus03 <yangarcus@gmail.com>
2026-05-11 18:13:32 +0800

    Add variables practice

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -1,1 +1,27 @@
+Python
+# This file is about Python variables
+
+# 1. String variable
+name = "Arcus Yang"
+
+# 2. Integer variable
+age = 23
+
+# 3. Float variable
+height = 1.88
+
+# 4. Boolean variable
+is_student = True
+
+# Print the variables
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# Print with explanation
+print("My name is", name)
+print("I am", age, "years old")
+print("My height is", height)
+print("Am I a student?", is_student)
 
